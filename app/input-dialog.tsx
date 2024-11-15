287db202afca9bbd9ce4fb6b5e01367f5971bec3
youssef aserrar <youssefasrar2966@gmail.com>
2024-11-15 23:18:13 +0100

    Create an input

---
+++ app/input-dialog.tsx
@@ -0,0 +1,195 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import ErrorMessage from "@/components/ui/error-message";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { inputSchema, InputSchema, inputTypes } from "@/validations/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
+import { v4 as uuid } from "uuid";
+import { FormType } from "./page";
+
+export const generateName = () => `name__${uuid().split("-")[1]}`;
+
+const defaultValues: InputSchema = {
+  name: generateName(),
+  type: "text",
+  label: "",
+  placeholder: "",
+  min: null,
+  max: null,
+} as const;
+
+type Props = {
+  setForm: React.Dispatch<React.SetStateAction<FormType[]>>;
+};
+
+const InputDialog = ({ setForm }: Props) => {
+  const {
+    handleSubmit,
+    control,
+    reset,
+    setValue,
+    register,
+    formState: { errors },
+  } = useForm<InputSchema>({
+    resolver: zodResolver(inputSchema),
+    defaultValues,
+  });
+
+  const onSubmit: SubmitHandler<InputSchema> = (data) => {
+    setForm((form) => [
+      ...form,
+      {
+        id: uuid(),
+        field: "input",
+        type: data.type,
+        name: data.name,
+        label: data.label,
+        placeholder: data.placeholder,
+      },
+    ]);
+  };
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button>Add input</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Input field</DialogTitle>
+        </DialogHeader>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="flex flex-col gap-2">
+            <div>
+              <Label htmlFor="type">Type</Label>
+              <Controller
+                control={control}
+                name="type"
+                render={({ field: { onBlur, onChange, ref, value } }) => (
+                  <Select
+                    onValueChange={(value) => {
+                      onChange(value);
+                      setValue("type", "text", {
+                        shouldValidate: true,
+                        shouldDirty: true,
+                        shouldTouch: true,
+                      });
+                    }}
+                    value={value}
+                  >
+                    <SelectTrigger id="type" onBlur={onBlur} ref={ref}>
+                      <SelectValue placeholder="Select type" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      {inputTypes.map((type) => (
+                        <SelectItem key={type} value={type}>
+                          {type}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                )}
+              />
+              <ErrorMessage>{errors.type?.message}</ErrorMessage>
+            </div>
+            <div>
+              <Label htmlFor="name">Name</Label>
+              <Input id="name" placeholder="Name" {...register("name")} />
+              <ErrorMessage>{errors.name?.message}</ErrorMessage>
+            </div>
+            <div>
+              <Label htmlFor="label">Label</Label>
+              <Input id="label" placeholder="Label" {...register("label")} />
+              <ErrorMessage>{errors.label?.message}</ErrorMessage>
+            </div>
+            <div>
+              <Label htmlFor="placeholder">Placeholder</Label>
+              <Input
+                id="placeholder"
+                placeholder="Placeholder"
+                {...register("placeholder")}
+              />
+              <ErrorMessage>{errors.placeholder?.message}</ErrorMessage>
+            </div>
+            <div className="flex gap-2">
+              <div className="flex-1">
+                <Label htmlFor="min">Min</Label>
+                <Controller
+                  control={control}
+                  name="min"
+                  render={({ field }) => (
+                    <Input
+                      id="min"
+                      placeholder="Min"
+                      {...field}
+                      value={field.value?.toString()}
+                      onChange={(e) =>
+                        field.onChange(
+                          e.target.value ? parseInt(e.target.value) : null
+                        )
+                      }
+                    />
+                  )}
+                />
+                <ErrorMessage>{errors.min?.message}</ErrorMessage>
+              </div>
+              <div className="flex-1">
+                <Label htmlFor="min">Max</Label>
+                <Controller
+                  control={control}
+                  name="max"
+                  render={({ field }) => (
+                    <Input
+                      id="max"
+                      placeholder="Max"
+                      {...field}
+                      value={field.value?.toString()}
+                      onChange={(e) =>
+                        field.onChange(
+                          e.target.value ? parseInt(e.target.value) : null
+                        )
+                      }
+                    />
+                  )}
+                />
+                <ErrorMessage>{errors.max?.message}</ErrorMessage>
+              </div>
+            </div>
+            <div className="flex gap-2 mt-2">
+              <Button className="flex-1" type="submit">
+                Add field
+              </Button>
+              <Button
+                className="flex-1"
+                variant="destructive"
+                type="button"
+                onClick={() =>
+                  reset({ ...defaultValues, name: generateName() })
+                }
+              >
+                Reset
+              </Button>
+            </div>
+          </div>
+        </form>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default InputDialog;
--- app/page.tsx
+++ app/page.tsx
@@ -4,95 +4,38 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
-import {
-  fieldTypes,
-  FormSchema,
-  formSchema,
-  inputTypes,
-} from "@/validations/form";
+import { TrashIcon, X } from "lucide-react";
 import { useState } from "react";
-import { Controller, SubmitHandler, useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
-import ErrorMessage from "@/components/ui/error-message";
 import { v4 as uuid } from "uuid";
-import { X } from "lucide-react";
+import InputDialog from "./input-dialog";
+import TextareaDialog from "./textarea-dialog";
 
-const generateName = () => `name__${uuid().split("-")[1]}`;
+export const generateName = () => `name__${uuid().split("-")[1]}`;
 
-type FormType =
+export type FormType =
   | {
       id: string;
-      type: "input" | "textarea";
-      inputType: "text" | "email" | "password" | "file";
+      field: "input";
+      type: "text" | "email" | "password" | "file";
       name: string;
       label: string;
       placeholder: string;
     }
   | {
       id: string;
-      type: "select";
+      field: "textarea";
       name: string;
       label: string;
-      options: { label: string; value: string }[];
-    }
-  | {
-      id: string;
-      type: "checkbox" | "switch";
-      name: string;
-      label: string;
+      placeholder: string;
     };
-
-const defaultValues: FormSchema = {
-  type: "input",
-  name: generateName(),
-  inputType: "text",
-  label: "",
-  placeholder: "",
-  min: null,
-  max: null,
-  options: [],
-} as const;
+// | {
+//     id: string;
+//     name: string;
+//     label: string;
+//   };
 
 export default function Home() {
   const [form, setForm] = useState<FormType[]>([]);
-
-  const {
-    handleSubmit,
-    control,
-    reset,
-    watch,
-    setValue,
-    register,
-    formState: { errors },
-  } = useForm<FormSchema>({
-    resolver: zodResolver(formSchema),
-    defaultValues,
-  });
-
-  const onSubmit: SubmitHandler<FormSchema> = (data) => {
-    if (data.type === "input" || data.type === "textarea") {
-      setForm([
-        ...form,
-        {
-          id: uuid(),
-          type: data.type,
-          inputType: data.inputType,
-          name: data.name,
-          label: data.label,
-          placeholder: data.placeholder,
-        },
-      ]);
-    }
-  };
-
-  const type = watch("type");
 
   const deleteField = (id: string) => {
     setForm(form.filter((f) => f.id !== id));
@@ -100,143 +43,42 @@ export default function Home() {
 
   return (
     <main className="max-w-7xl mx-auto p-4">
+      <section className="p-4 rounded-lg border border-input flex gap-2 mb-2">
+        <InputDialog setForm={setForm} />
+        <TextareaDialog setForm={setForm} />
+      </section>
       <section className="grid grid-cols-2 gap-4">
         <div className="p-4 rounded-lg border border-input flex flex-col gap-2">
-          <h1 className="text-2xl font-bold">Form builder</h1>
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <div>
-              <Label htmlFor="type">Type</Label>
-              <Controller
-                control={control}
-                name="type"
-                render={({ field: { onBlur, onChange, ref, value } }) => (
-                  <Select
-                    onValueChange={(value) => {
-                      onChange(value);
-                      setValue("inputType", "text", {
-                        shouldValidate: true,
-                        shouldDirty: true,
-                        shouldTouch: true,
-                      });
-                    }}
-                    value={value}
-                  >
-                    <SelectTrigger id="type" onBlur={onBlur} ref={ref}>
-                      <SelectValue placeholder="Select type" />
-                    </SelectTrigger>
-                    <SelectContent>
-                      {fieldTypes.map((type) => (
-                        <SelectItem key={type} value={type}>
-                          {type}
-                        </SelectItem>
-                      ))}
-                    </SelectContent>
-                  </Select>
-                )}
-              />
-              <ErrorMessage>{errors.type?.message}</ErrorMessage>
-            </div>
-            {type === "input" && (
-              <div>
-                <Label htmlFor="type">Input type</Label>
-                <Controller
-                  control={control}
-                  name="inputType"
-                  render={({ field: { onBlur, onChange, ref, value } }) => (
-                    <Select
-                      onValueChange={(value) => onChange(value)}
-                      value={value ?? "text"}
-                    >
-                      <SelectTrigger id="inputType" onBlur={onBlur} ref={ref}>
-                        <SelectValue placeholder="Select input type" />
-                      </SelectTrigger>
-                      <SelectContent>
-                        {inputTypes.map((type) => (
-                          <SelectItem key={type} value={type}>
-                            {type}
-                          </SelectItem>
-                        ))}
-                      </SelectContent>
-                    </Select>
-                  )}
-                />
-                <ErrorMessage>{errors.inputType?.message}</ErrorMessage>
-              </div>
-            )}
-            <div>
-              <Label htmlFor="name">Name</Label>
-              <Input id="name" placeholder="Name" {...register("name")} />
-              <ErrorMessage>{errors.name?.message}</ErrorMessage>
-            </div>
-            <div>
-              <Label htmlFor="label">Label</Label>
-              <Input id="label" placeholder="Label" {...register("label")} />
-              <ErrorMessage>{errors.label?.message}</ErrorMessage>
-            </div>
-            {(type === "input" || type === "textarea") && (
-              <>
-                <div>
-                  <Label htmlFor="placeholder">Placeholder</Label>
-                  <Input
-                    id="placeholder"
-                    placeholder="Placeholder"
-                    {...register("placeholder")}
-                  />
-                  <ErrorMessage>{errors.placeholder?.message}</ErrorMessage>
-                </div>
-                <div className="flex gap-2">
-                  <div className="flex-1">
-                    <Label htmlFor="min">Min</Label>
-                    <Input id="min" placeholder="Min" {...register("min")} />
-                    <ErrorMessage>{errors.min?.message}</ErrorMessage>
-                  </div>
-                  <div className="flex-1">
-                    <Label htmlFor="min">Max</Label>
-                    <Input id="max" placeholder="Max" {...register("max")} />
-                    <ErrorMessage>{errors.max?.message}</ErrorMessage>
-                  </div>
-                </div>
-              </>
-            )}
-
-            <div className="flex gap-2 mt-2">
-              <Button className="flex-1" type="submit">
-                Add field
-              </Button>
-              <Button
-                className="flex-1"
-                variant="destructive"
-                type="button"
-                onClick={() =>
-                  reset({ ...defaultValues, name: generateName() })
-                }
-              >
-                Reset
-              </Button>
-            </div>
-          </form>
-        </div>
-        <div className="p-4 rounded-lg border border-input flex flex-col gap-2">
           {form.map((item) => (
-            <div key={item.id} className="group relative">
+            <div
+              key={item.id}
+              className="flex justify-between p-2 border border-input rounded-lg items-center"
+            >
+              <p>{item.field}</p>
               <Button
                 variant="ghost"
                 size="icon"
-                className="h-6 w-6 group-hover:flex absolute top-0 right-0 hidden hover:bg-red-100"
+                className="h-8 w-8 hover:bg-red-100"
                 onClick={() => deleteField(item.id)}
               >
-                <X size={12} className="text-red-500" />
+                <TrashIcon size={12} className="text-red-500" />
               </Button>
+            </div>
+          ))}
+        </div>
+        <div className="p-4 rounded-lg border border-input flex flex-col gap-2">
+          {form.map((item) => (
+            <div key={item.id}>
               <Label htmlFor={item.name}>{item.label}</Label>
-              {item.type === "input" && (
+              {item.field === "input" && (
                 <Input
                   id={item.name}
                   name={item.name}
-                  type={item.inputType}
+                  type={item.type}
                   placeholder={item.placeholder}
                 />
               )}
-              {item.type === "textarea" && (
+              {item.field === "textarea" && (
                 <Textarea
                   id={item.name}
                   name={item.name}
@@ -268,7 +110,7 @@ return (
           </pre>
 
           {form.map((item) => {
-            if (item.type === "input") {
+            if (item.field === "input") {
               return (
                 <pre key={item.id}>
                   {`
@@ -282,7 +124,7 @@ return (
               );
             }
 
-            if (item.type === "textarea") {
+            if (item.field === "textarea") {
               return (
                 <pre key={item.id}>
                   {`
